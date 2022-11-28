1e43c383637fc3e83f55f42681253f1b65a6ae49
Adan Millard <adanmillard@gmail.com>
2022-11-28 16:59:27 +1300

    homepage body set up

--- src/Components/HomepageBody/HomepageBody.tsx
+++ src/Components/HomepageBody/HomepageBody.tsx
@@ -3,7 +3,8 @@ import "./homepageBody.css";
 export const HomepageBody = () => {
   return (
     <div className="homepage-body-container">
-      <h1>It's Me Adan</h1>
+      <h1>It's Me Adan.</h1>
+      <p>Let's continue...</p>
     </div>
   );
 };
